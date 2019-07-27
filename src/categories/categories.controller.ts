2db71bcf4b964713956983129a0ddbedc850842a
Paweł Kaczmarek <pawel.kaczmarek95@gmail.com>
2019-07-27 21:10:59 +0200

    End categories feature

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,19 +1,20 @@
-import { Controller, Get, Post, Delete, Patch, Param, ParseIntPipe, Body, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Patch, Param, ParseIntPipe, Body, UsePipes, ValidationPipe, Query } from '@nestjs/common';
 import { CreatePostDto } from 'src/posts/dto/create-post.dto';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { CategoriesService } from './categories.service';
 import { Category } from './category.entity';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { GetCategoriesFilterDto } from './dto/get-categories-filter.dto';
 
 @Controller('category')
 export class CategoriesController {
 
   constructor(private categoryService: CategoriesService) {}
 
-  // @Get()
-  // getCategories() {
-  //   return 'Dupa';
-  // }
+  @Get()
+  getCategories(@Query(ValidationPipe) filterCategoriesDto: GetCategoriesFilterDto): Promise<Category[]> {
+   return this.categoryService.getCategories(filterCategoriesDto);
+  }
 
   @Get('/:id')
   getCategoryById(@Param('id', ParseIntPipe) id: number) {
