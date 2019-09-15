4753c13eda32e4975743d0aada766005bf7895bf
Paweł Kaczmarek <pawel.kaczmarek95@gmail.com>
2019-09-15 16:30:25 +0200

    Add password hash feature

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,8 @@
 import { Repository, EntityRepository } from "typeorm";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
-import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -10,8 +11,9 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
-
+    user.salt = await bcrypt.genSalt();;
+    user.password = await this.hashPassword(password, user.salt);
+    
     try {
       await user.save();
     } catch(error) {
@@ -21,5 +23,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
