5126d252b34d70e32dcdf1d519bee4852885ba11
Paweł Kaczmarek <pawel.kaczmarek95@gmail.com>
2019-09-15 17:12:18 +0200

    Add simple signIn feature

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -13,7 +13,7 @@ export class UserRepository extends Repository<User> {
     user.username = username;
     user.salt = await bcrypt.genSalt();;
     user.password = await this.hashPassword(password, user.salt);
-    
+
     try {
       await user.save();
     } catch(error) {
@@ -22,6 +22,17 @@ export class UserRepository extends Repository<User> {
       } else {
         throw new InternalServerErrorException();
       }
+    }
+  }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+
+    if (user && await user.validatePassword(password)) {
+      return user.username;
+    } else {
+      return null;
     }
   }
 
