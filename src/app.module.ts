7262ca9e0f6acb2b993fd1ad5ca994d996a875bd
Paweł Kaczmarek <pawel.kaczmarek95@gmail.com>
2019-09-15 14:08:54 +0200

    Add base structure for auth feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { CategoriesModule } from './categories/categories.module';
-
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     PostsModule,
-    CategoriesModule
+    CategoriesModule,
+    AuthModule
   ],
   providers: [],
 })
